48a7a7804b9562527ad8e5f963b9c74db83d80f4
Btike <111445261+Btike@users.noreply.github.com>
2025-05-24 18:40:33 -0400

    fix

--- MyUnits-Vue/src/App.vue
+++ MyUnits-Vue/src/App.vue
@@ -4,9 +4,9 @@ import Converter from './components/Converter.vue';
 
 <template>
   <div class="content-wrapper">
-    <header>
+    <header class="header">
       <h1 class="title">MyUnits</h1>
-      <h3>
+      <h3 class="subtitle">
         Un convertidor de unidades para todas tus necesidades, solo escoge la unidad y la cantidad, y el convertidor hará el resto.
       </h3>
     </header>
@@ -20,13 +20,12 @@ import Converter from './components/Converter.vue';
 <style scoped>
 
 /* Header: texto justificado al ancho del wrapper */
-.header {
-  line-height: 1.5;
-}
+
 .title {
-  align-items: center;
   text-align: center;
 }
-
+.subtitle {
+  text-align: justify;
+}
 /* Converter heredará el 100% de ancho de .content-wrapper */
 </style>
