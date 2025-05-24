428e2fd92953b6ce8fa91c6cb825babb4b405fc8
Btike <111445261+Btike@users.noreply.github.com>
2025-05-24 22:41:11 +0000

    fix(css-styles)

--- MyUnits-Vue/src/App.vue
+++ MyUnits-Vue/src/App.vue
@@ -1,56 +1,32 @@
 <script setup>
 import Converter from './components/Converter.vue';
-
-
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <h1>MyUnits</h1>
-      <p>
-        Un convertidor de unidades para todas tus necesidades.
-      </p>
-      <p>
-        Solo selecciona tu magnitud y luego las unidades que deseas convertir.
-        <br />
-        El convertidor se encargará del resto.
-        <br />
-      </p>
-    </div>
-  </header>
+  <div class="content-wrapper">
+    <header>
+      <h1 class="title">MyUnits</h1>
+      <h3>
+        Un convertidor de unidades para todas tus necesidades, solo escoge la unidad y la cantidad, y el convertidor hará el resto.
+      </h3>
+    </header>
 
-  <main>
-    <Converter />
-  </main>
+    <main>
+      <Converter />
+    </main>
+  </div>
 </template>
 
 <style scoped>
-header {
+
+/* Header: texto justificado al ancho del wrapper */
+.header {
   line-height: 1.5;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.title {
+  align-items: center;
+  text-align: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    flex-direction: column;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+/* Converter heredará el 100% de ancho de .content-wrapper */
 </style>
